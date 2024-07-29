d72ba623eead34a2252ddac4f7347b90902da2be
azmddy <azmddy@163.com>
2024-07-29 19:39:34 +0800

    fix and load model

--- dqn.py
+++ dqn.py
@@ -1,6 +1,8 @@
 import logging
 import platform
 import random
+from enum import Enum
+
 import numpy as np
 import collections
 import torch
@@ -20,7 +22,7 @@ class ReplayBuffer:
     def sample(self, batch_size):  # 从buffer中采样数据,数量为batch_size
         transitions = random.sample(self.buffer, batch_size)
         state, action, reward, next_state, done = zip(*transitions)
-        return np.array(state), action, reward, np.array(next_state), done
+        return state, action, reward, next_state, done
 
     def size(self):  # 目前buffer中数据的数量
         return len(self.buffer)
@@ -73,13 +75,13 @@ class Qnet(torch.nn.Module):
         super(Qnet, self).__init__()
         # 输入层 -> 隐藏层
         self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
-        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        # self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
         # 隐藏层 -> 输出层
         self.fc3 = torch.nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         return self.fc3(x)
 
 
@@ -89,25 +91,31 @@ class VAnet(torch.nn.Module):
     def __init__(self, state_dim, hidden_dim, action_dim):
         super(VAnet, self).__init__()
         self.fc1 = torch.nn.Linear(state_dim, hidden_dim)  # 共享网络部分
-        self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
+        # self.fc2 = torch.nn.Linear(hidden_dim, hidden_dim)
 
         self.fc_A = torch.nn.Linear(hidden_dim, action_dim)
         self.fc_V = torch.nn.Linear(hidden_dim, 1)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        # x = F.relu(self.fc2(x))
         A = self.fc_A(x)
         V = self.fc_V(x)
         Q = V + A - A.mean(1).view(-1, 1)  # Q值由V值和A值计算得到
         return Q
 
 
+class DqnType(Enum):
+    VANILLA_DQN = 1
+    DUELING_DQN = 2
+    DOUBLE_DQN = 3
+
+
 class DQN:
     """ DQN算法 """
 
-    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma,
-                 epsilon, target_update, device, dqn_type='VanillaDQN'):
+    def __init__(self, state_dim, hidden_dim, action_dim, learning_rate, gamma, epsilon, epsilon_decay, target_update,
+                 device, dqn_type=DqnType.VANILLA_DQN, is_eval=False):
         """
         初始化qdn网络
         :param state_dim: 输入层-状态空间 维度
@@ -115,29 +123,36 @@ class DQN:
         :param action_dim: 输出层-动作空间 维度(表示有几个动作)
         :param learning_rate: 学习率
         :param gamma: 折扣因子
-        :param epsilon: 贪婪策略
+        :param epsilon: 贪婪策略(探索因子)
+        :param epsilon_decay: 探索衰减因子
         :param target_update: 目标网络更新频率
         :param device: gpu or cpu
+        :param dqn_type: dqn网络类型
+        :param is_eval: 是否是评测模式
         """
         self.action_dim = action_dim
-        if dqn_type == 'DuelingDQN':  # Dueling DQN采取不一样的网络框架
-            self.q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
-            self.target_q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
+        if not is_eval:
+            if dqn_type == DqnType.DUELING_DQN:  # Dueling DQN采取不一样的网络框架
+                self.q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
+                self.target_q_net = VAnet(state_dim, hidden_dim, self.action_dim).to(device)
+            else:
+                self.q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+                self.target_q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
+            # 使用Adam优化器
+            self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr=learning_rate)
         else:
-            self.q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
-            self.target_q_net = Qnet(state_dim, hidden_dim, self.action_dim).to(device)
-        # 使用Adam优化器
-        self.optimizer = torch.optim.Adam(self.q_net.parameters(),
-                                          lr=learning_rate)
+            self.load_model()
         self.gamma = gamma  # 折扣因子
         self.epsilon = epsilon  # epsilon-贪婪策略
         self.target_update = target_update  # 目标网络更新频率
         self.count = 0  # 计数器,记录更新次数
         self.device = device
         self.dqn_type = dqn_type
+        self.epsilon_decay = epsilon_decay  # 探索衰减因子
+        self.is_eval = is_eval
 
     def take_action(self, state):  # epsilon-贪婪策略采取动作
-        if np.random.random() < self.epsilon:
+        if np.random.random() < self.epsilon and not self.is_eval:
             # 随机探索
             action = np.random.randint(self.action_dim)
         else:
@@ -154,7 +169,7 @@ class DQN:
 
         q_values = self.q_net(states).gather(1, actions)  # Q值
         # 下个状态的最大Q值
-        if self.dqn_type == 'DoubleDQN':
+        if self.dqn_type == DqnType.DOUBLE_DQN:
             max_action = self.q_net(next_states).max(1)[1].view(-1, 1)
             max_next_q_values = self.target_q_net(next_states).gather(1, max_action)
         else:
@@ -170,47 +185,51 @@ class DQN:
             self.target_q_net.load_state_dict(self.q_net.state_dict())  # 更新目标网络
         self.count += 1
 
+    def save_model(self):
+        torch.save(self.q_net, 'dqn_q_net_model.pth')
+        torch.save(self.target_q_net, 'dqn_target_q_net_model.pth')
+        torch.save(self.optimizer, 'dqn_optimizer_model.pth')
+
+    def load_model(self):
+        self.q_net = torch.load('dqn_q_net_model.pth')
+        self.target_q_net = torch.load('dqn_target_q_net_model.pth')
+        self.optimizer = torch.load('dqn_optimizer_model.pth')
+
 
 # 定义环境
 class SysEnv:
     def __init__(self, grid_width, grid_height):
         self.grid_width = grid_width
         self.grid_height = grid_height
-        self.state = [self.__pos2int(0, 0)]
-        self.goal = [self.__pos2int(grid_width - 1, grid_height - 1)]
+        self.state = (0, 0)
+        self.goal = (grid_width - 1, grid_height - 1)
 
     def reset(self):
-        self.state = [self.__pos2int(0, 0)]
+        self.state = (0, 0)
         return self.state
 
     def step(self, action):
-        x, y = self.__int2pos(self.state[0])
+        x, y = self.state
         if action == 0:  # 上
             y = min(self.grid_height - 1, y + 1)
         elif action == 1:  # 下
             y = max(0, y - 1)
         elif action == 2:  # 左
-            x = min(0, x - 1)
+            x = max(0, x - 1)
         elif action == 3:  # 右
-            x = max(self.grid_width - 1, x + 1)
+            x = min(self.grid_width - 1, x + 1)
         old_state = self.state
-        self.state = [self.__pos2int(x, y)]
+        self.state = (x, y)
+        reward = -1
+        if old_state == self.state:
+            reward += -50
         done = self.state == self.goal
         if done:
-            reward = 1000
+            reward += 1000
         else:
-            if old_state == self.state:
-                reward = -10
-            else:
-                reward = -(abs(self.__int2pos(self.goal[0])[0] - x) + abs(self.__int2pos(self.goal[0])[1] - y))
+            goal_x, goal_y = self.goal
+            reward += - (abs(goal_x - x) + abs(goal_y - y))
         return self.state, reward, done
-
-    def __pos2int(self, x: int, y: int):
-        """二维坐标转一维"""
-        return self.grid_width * y + x
-
-    def __int2pos(self, posval: int):
-        return posval % self.grid_width, posval / self.grid_width
 
 
 def train_dqn(epochs=1000):
@@ -221,14 +240,16 @@ def train_dqn(epochs=1000):
         device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
     replay_buffer = ReplayBuffer(2000)
-    state_dim = 1
+    state_dim = 2
     action_dim = 4
     hidden_dim = 64
     lr = 0.001
     gamma = 0.95
     epsilon = 0.9
+    epsilon_decay = 0.995
     target_update = 10
-    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, target_update, device, "DuelingDQN")
+    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, epsilon_decay, target_update, device,
+                DqnType.VANILLA_DQN)
     env = SysEnv(10, 10)
     rewards = []
     for e in range(epochs):
@@ -258,7 +279,37 @@ def train_dqn(epochs=1000):
                 agent.update(transition_dict)
 
         rewards.append(total_reward)
+        if agent.epsilon > 0.01:
+            agent.epsilon *= agent.epsilon_decay
         if e % 10 == 0:
             print(f'Epoch: {e}, Total Reward: {total_reward}')
-
+    agent.save_model()
     return rewards
+
+
+def usc_dqn():
+    system_name = platform.system()
+    if system_name == "Darwin":
+        device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+    else:
+        device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+    state_dim = 2
+    action_dim = 4
+    hidden_dim = 64
+    lr = 0.001
+    gamma = 0.95
+    epsilon = 0.9
+    epsilon_decay = 0.995
+    target_update = 10
+    agent = DQN(state_dim, hidden_dim, action_dim, lr, gamma, epsilon, epsilon_decay, target_update, device,
+                DqnType.VANILLA_DQN, True)
+    env = SysEnv(10, 10)
+    state = env.reset()
+    done = False
+    while not done:
+        action = agent.take_action(state)
+        # 根据ai选择的动作 计算下个下一个状态和当前动作的奖励，以及是否完成任务
+        next_state, reward, done = env.step(action)
+        state = next_state
+        logging.error("pos {}, action: {}".format(state, action))
